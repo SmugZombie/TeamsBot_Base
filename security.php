7c91098501862bb39702f868befbe3432dfc5358
scrptd <ron@r-egli.com>
2020-02-05 18:03:21 +0000

    Allowed for multiple auth keys / multiple bots

--- security.php
+++ security.php
@@ -6,12 +6,22 @@ function checkAuthHeader(){
 	$a = getallheaders();
 	$provided_hmac=substr($a['Authorization'],5);
 
-	$hash = hash_hmac("sha256",file_get_contents('php://input'),base64_decode($config['teams']['secret']), true);
+	for ($i=0; $i < count($config['teams']['secrets']); $i++) { 
+		$hash = hash_hmac("sha256",file_get_contents('php://input'),base64_decode($config['teams']['secrets'][$i]), true);
 
-	$calculated_hmac = base64_encode($hash);
+		$calculated_hmac = base64_encode($hash);
+
+		if(hash_equals($provided_hmac,$calculated_hmac)){
+			return true;
+		}
+	}
+
+	return false;
+	
 	// Validate the message came from an authorized source
 	/*if(!hash_equals($provided_hmac,$calculated_hmac)){
 		return false;
 	}*/
-	return hash_equals($provided_hmac,$calculated_hmac);
+
+	//return hash_equals($provided_hmac,$calculated_hmac);
 }
